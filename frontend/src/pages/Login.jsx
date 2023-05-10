b63dcaed0ad9771b8ff6c28e7512d22d0c071008
Protick Saha <85408091+Protick10@users.noreply.github.com>
2023-05-10 11:25:00 +0600

    login and register finished

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Form, FormGroup, Button} from 'reactstrap';
 import {Link} from 'react-router-dom'
 import '../styles/login.css'
 
-import loginImg from '../assets/images/login.png';
-import userIcon from '../assets/images/user.png'
+import loginImg from './../assets/images/login.png';
+import userIcon from './../assets/images/user.png'
 
 const Login = () => {
-    return  
-    <section>
+
+    const[credentials, setCredentials] = useState({
+       email: undefined,
+       password: undefined
+    });
+
+
+    const handleChange = e => {
+        setCredentials(prev=>({...prev, [e.target.id]:e.target.value}))
+    };
+
+    const handleClick = e=>{
+        e.preventDefault()
+    }
+
+    return (
+        <section>
         <Container>
             <Row>
                 <Col lg='8' className='m-auto'>
                 <div className="login_container d-flex justify-content-between">
                     <div className="login_img">
-                        <img src= "E:\9th SEMESTER(SPRING-2023)\CSE-412\412 project\GhoraGhuri-A-tour-management-service\frontend\src\assets\images\login.png"  />
+                        <img src={loginImg} />
+                    </div>
+                    <div className="login_form">
+                        <div className="user">
+                            <img src={userIcon} alt="" />
+                        </div>
+                        <h2>Login</h2>
+
+                        <Form onSubmit={handleClick}>
+                            <FormGroup>
+                                <input type="email" 
+                                placeholder='Email'
+                                required id='email'
+                                onChange={handleChange} />
+                            </FormGroup>
+                            <FormGroup>
+                                <input type="password" 
+                                placeholder='Password' 
+                                required id='password'
+                                onChange={handleChange} />
+                            </FormGroup>
+                            <Button className='btn secondary_btn auth_btn'
+                            type='Submit'>Login</Button>
+                        </Form>
+                        <p>Don't have an accont? <Link to='/register'>Create</Link></p>
                     </div>
                 </div>
                 </Col>
             </Row>
         </Container>
-    </section>
+     </section>
+
+    ) 
+
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, FormGroup, Button} from 'reactstrap';
+import {Link} from 'react-router-dom'
+import '../styles/login.css'
+
+import registerImg from './../assets/images/register.png';
+import userIcon from './../assets/images/user.png'
 
 const Register = () => {
+
+    const[credentials, setCredentials] = useState({
+        userName:undefined,
+       email: undefined,
+       password: undefined
+    });
+
+
+    const handleChange = e => {
+        setCredentials(prev=>({...prev, [e.target.id]:e.target.value}))
+    };
+
+    const handleClick = e=>{
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            Register
-        </div>
-    );
+        <section>
+        <Container>
+            <Row>
+                <Col lg='8' className='m-auto'>
+                <div className="login_container d-flex justify-content-between">
+                    <div className="login_img">
+                        <img src={registerImg} />
+                    </div>
+                    <div className="login_form">
+                        <div className="user">
+                            <img src={userIcon} alt="" />
+                        </div>
+                        <h2>Register</h2>
+
+                        <Form onSubmit={handleClick}>
+                            <FormGroup>
+                                <input type="text" 
+                                placeholder='Username'
+                                required id='username'
+                                onChange={handleChange} />
+                            </FormGroup>
+                            <FormGroup>
+                                <input type="email" 
+                                placeholder='Email'
+                                required id='email'
+                                onChange={handleChange} />
+                            </FormGroup>
+                            <FormGroup>
+                                <input type="password" 
+                                placeholder='Password' 
+                                required id='password'
+                                onChange={handleChange} />
+                            </FormGroup>
+                            <Button className='btn secondary_btn auth_btn'
+                            type='Submit'>Create Account</Button>
+                        </Form>
+                        <p>Already have an accont? <Link to='/login'>Login</Link></p>
+                    </div>
+                </div>
+                </Col>
+            </Row>
+        </Container>
+     </section>
+
+    ) 
+
 };
 
 export default Register;
+

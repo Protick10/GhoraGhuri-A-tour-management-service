2855a1f1334effe2f0d0c82c8f7697843a5b41aa
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2023-05-19 05:45:02 +0600

    tourconrolller.js, tours.js, env and tours.json updated

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,11 +1,32 @@
 
 import express from 'express'
-import { createTour } from '../controllers/tourController.js'
+import { createTour, deleteTour, getSingleTour, getAllTour, updateTour, getTourBySearch, getFeaturedTour, getTourCount } from '../controllers/tourController.js'
 
 const router = express.Router()
 
 // create new tour 
-router.post('/', createTour)
+router.post('/', createTour);
+
+// update tour 
+router.put('/:id', updateTour);
+
+// delete tour 
+router.delete('/:id', deleteTour);
+
+// get single tour 
+router.get('/:id', getSingleTour);
+
+// get all tour 
+router.get('/', getAllTour);
+
+// get tour by search 
+router.get("/search/getTourBySearch",getTourBySearch);
+
+// get Featured tour 
+router.get("/search/getFeaturedTours", getFeaturedTour);
+
+// get Tour count
+router.get("/search/getTourCount", getTourCount);
 
 
 export default router;

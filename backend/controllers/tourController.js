f2586a5f9ff9c0f64b038e02e0fe90a829cc409f
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2023-05-19 11:01:06 +0600

    create review.js and reviewController

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -58,7 +58,7 @@ export const getSingleTour = async(req,res)=>{
 
     try {
         
-        const tour = await Tour.findById(id);
+        const tour = await Tour.findById(id).populate("reviews");
 
         res.status(200).json({success:true, message:"successful", data: tour,});
         
@@ -75,7 +75,7 @@ export const getAllTour = async(req,res)=>{
         const page =parseInt(req.query.page)
     
     try {
-        const tours = await Tour.find({}).skip(page * 8).limit(8);
+        const tours = await Tour.find({}).populate("reviews").skip(page * 8).limit(8);
 
         res.status(200).json({success:true, count: tours.length, message:"successful", data: tours,});
 
@@ -96,7 +96,7 @@ export const getTourBySearch = async(req,res)=>{
     try {
 
         // gte means greater than equal
-        const tours = await Tour.find({city, distance:{$gte:distance}, maxGroupSize:{$gte:maxGroupSize}})
+        const tours = await Tour.find({city, distance:{$gte:distance}, maxGroupSize:{$gte:maxGroupSize}}).populate("reviews")
         
         res.status(200).json({success:true, message:"successful", data: tours,});
 
@@ -111,7 +111,7 @@ export const getTourBySearch = async(req,res)=>{
 export const getFeaturedTour = async(req,res)=>{
 
 try {
-    const tours = await Tour.find({featured:true}).limit(8);
+    const tours = await Tour.find({featured:true}).populate("reviews").limit(8);
 
     res.status(200).json({success:true, message:"successful", data: tours,});
 

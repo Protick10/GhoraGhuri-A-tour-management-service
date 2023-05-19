0c902c03399b210f848d67b1b9501d1ed576d31a
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2023-05-19 10:07:33 +0600

    Created verifyToken, Password hashing done and authentication completed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -28,9 +28,38 @@ export const register = async (req,res) =>{
 
 // user login
 export const login = async (req,res) =>{
+
+        const email = req.body.email
+
     try {
-        
+
+        const user = await User.findOne({email})
+
+        // if user doesn't exist
+        if(!user){
+            return res.status(404).json({success:false, message: "user not found"})
+        }
+
+        // if user exist then check the password
+        const checkCorrectPassword = await bcrypt.compare(req.body.password, user.password)
+
+        if(!checkCorrectPassword){
+            return res.status(401).json({success:false, message:"Incorrect Email or Password"})
+        }
+
+        const {password, role, ...rest} = user._doc
+
+        // create jwt token
+        const token = Jwt.sign({id:user._id, role:user.role}, process.env.JWT_SECRET_KEY, {expiresIn: "15d"});
+
+
+        // set token in the browser cookies and send the response to the client
+        res.cookie('accessToken',token,{
+            httpOnly: true,
+            expires:token.expiresIn,
+        }).status(200).json({token, data: { ...rest },role,})
+         
     } catch (err) {
-        
+        res.status(500).json({success:false, message:"Failed to login"});
     }
 };
